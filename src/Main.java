43c6c6030eaad85e1a0efe17f0120b365ca20ed2
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2023-09-19 23:54:49 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221)); // true
+        System.out.println(isPalindrome(707));   // true
+        System.out.println(isPalindrome(11212)); // false
+    }
+
+    public static boolean isPalindrome(int number) {
+        if (number < 0) {
+            return false;
+        }
+
+        int originalNumber = number;
+        int reverseNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reverseNumber = reverseNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return originalNumber == reverseNumber;
     }
 }
